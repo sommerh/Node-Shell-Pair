c638867e2ab8b7a626acb2e03c98acd83889409f
Yeji Jang <yejijang95@gmail.com>
2022-03-07 17:56:06 -0500

    .

--- bash.js
+++ bash.js
@@ -1,12 +1,17 @@
+const pwdExport = require("./pwd");
+const lsExport = require("./ls");
+const catExport = require("./cat");
+
 process.stdout.write("prompt > ");
 
 process.stdin.on("data", (data) => {
   const cmd = data.toString().trim();
-
   if (cmd === "pwd") {
-    // process.stdout.write(__dirname);
-    process.stdout.write(process.cwd());
-    process.stdout.write("\nprompt > ");
+    pwdExport();
+  } else if (cmd === "ls") {
+    lsExport();
+  } else if (cmd === "cat") {
+    catExport();
   } else {
     process.stdout.write("You typed: " + cmd);
     process.stdout.write("\nprompt > ");
